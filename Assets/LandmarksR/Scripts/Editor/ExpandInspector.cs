2f5cb4ff2dcab2deaefcb4cf79e17a0d34dde7ec
greasycat <131315c@gmail.com>
2024-08-08 17:18:36 -0400

    Remove resume utility and move editor shortcut

--- Assets/LandmarksR/Scripts/Editor/ExpandInspector.cs
+++ Assets/LandmarksR/Scripts/Editor/ExpandInspector.cs
@@ -3,20 +3,31 @@ using UnityEditor;
 
 namespace LandmarksR.Scripts.Editor
 {
-    public class ExpandInspector: EditorWindow
+    public static class ExpandInspector
     {
         /// <summary>
         /// Expands the selected GameObject in the Hierarchy window.
         /// </summary>
-        [MenuItem("EditorUtility/Shortcuts/Expand Selected Tasks &q")]
+        [MenuItem("LandmarksR/Shortcuts/Expand Selected Tasks &q")]
         //https://stackoverflow.com/a/66366775
         private static void ExpandTasks()
         {
+            if (Selection.activeGameObject == null)
+            {
+                return;
+            }
+
             var type = typeof(EditorWindow).Assembly.GetType("UnityEditor.SceneHierarchyWindow");
-            var window = GetWindow(type);
+            var window = EditorWindow.GetWindow(type);
             var expandCallback = type.GetMethod("SetExpandedRecursive");
             if (expandCallback != null)
                 expandCallback.Invoke(window, new object[] {Selection.activeGameObject.GetInstanceID(), true});
+        }
+
+        [MenuItem("LandmarksR/Shortcuts/Expand Selected Tasks &q", true)]
+        private static bool ValidateExpandTasks()
+        {
+            return Selection.activeGameObject != null;
         }
     }
 }
